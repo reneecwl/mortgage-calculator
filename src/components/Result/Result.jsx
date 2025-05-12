87817a0ad4b9f670bc207bbc298505bd704be266
reneecwl <reneewlcheng@gmail.com>
2025-05-12 00:26:38 -0400

    Update result box styling

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -5,13 +5,13 @@ export default function Results({ monthlyPayment, year, interestPayment }) {
   return (
     <div className="results">
       {monthlyPayment || interestPayment ? (
-        <>
+        <div className="payment">
           <h3 className="payment__header">Your results</h3>
           <p className="payment__content">
             Your results are shown below based on the information you provided. To adjust the results, edit the form and
-            click “calculate repayments” again. Your monthly repayments Total you'll repay over the term
+            click “calculate repayments” again.
           </p>
-          <div className="payment">
+          <div className="payment__box">
             <div className="payment__monthly">
               Your Monthly repayments
               <span className="payment__monthly-amount">£{monthlyPayment}</span>
@@ -21,7 +21,7 @@ export default function Results({ monthlyPayment, year, interestPayment }) {
               <span className="payment__total-amount">£{{ monthlyPayment } * { year } * 12}</span>
             </div>
           </div>
-        </>
+        </div>
       ) : (
         <div className="results__no-result">
           <img className="results__image" src={illustration} alt="illustration" />
